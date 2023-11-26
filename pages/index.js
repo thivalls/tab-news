ef9dcdbaf8923299144fb725bb29c924eb26c2e1
Thiago Valls Bertolino <olavalls@gmail.com>
2023-11-26 04:48:59 +0000

    add new phrase to test

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="App">
+            <h1>Eu te amo muito!!!</h1>
+            <p>Obrigado por tudo que faz por mim e pelo Théo.</p>
+        </div>
+    )
+}
+
+export default Home;
