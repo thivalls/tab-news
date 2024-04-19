b14c52364065ea995d277a273ce7e7d2f3899322
valls <olavalls@gmail.com>
2024-04-18 22:58:48 -0300

    improving database to be more robust for errors

--- infra/database.js
+++ infra/database.js
@@ -10,6 +10,14 @@ async function query(queryObject) {
   });
   await client.connect();
 
+  console.log("Database Connection Config", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  })
+
   try {
     const result = await client.query(queryObject);
     return result;
