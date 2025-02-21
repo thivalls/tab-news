ea7c834b1cbddbd47911510567321fb47751d4db
valls <olavalls@gmail.com>
2025-02-20 22:32:01 -0300

    adiciona inicio para teste da migration live run

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,12 @@
+describe("# Suite for API integration", () => {
+  it("should POST api/v1/migrations and return 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: "POST",
+    });
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    console.log(responseBody);
+    expect(Array.isArray(responseBody)).toBeTruthy();
+  });
+});
